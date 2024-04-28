7f923c9c1caaf60ff00c53352d3566e800f823b4
vetonmilaimi <vetonmila@gmail.com>
2024-04-28 23:31:58 +0200

    Made code structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Header from './components/header'
+import Card from './components/ui/Card'
+
+function App() {
+	return (
+		<>
+			<Header />
+			<Card />
+		</>
+	)
+}
+
+export default App
